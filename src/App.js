a15737b6922908498c78ca753616d28090c475d3
Dylan Peltier <dylandavidpeltier@gmail.com>
2022-08-09 09:59:28 -0400

    added React Lazy Loading with Suspense

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import Layout from './components/Layout';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Gallery from './pages/Gallery';
+// import Home from './pages/Home';
+// import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Gallery from './pages/Gallery';
+const About = React.lazy(() => import('./pages/About'));
+const Gallery = React.lazy(() => import('./pages/Gallery'));
+const Home = React.lazy(() => import('./pages/Home'));
+const Contact = React.lazy(() => import('./pages/Contact'));
 
 function App() {
 	return (
 		<Layout>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/contact-us" element={<Contact />} />
-				<Route path="/about-us" element={<About />} />
-				<Route path="/gallery" element={<Gallery />} />
+				<Route
+					path="/contact-us"
+					element={
+						<Suspense fallback={<div>Loading...</div>}>
+							<Contact />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/about-us"
+					element={
+						<Suspense fallback={<div>Loading...</div>}>
+							<About />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/gallery"
+					element={
+						<Suspense fallback={<div>Loading...</div>}>
+							<Gallery />
+						</Suspense>
+					}
+				/>
 			</Routes>
 		</Layout>
 	);
