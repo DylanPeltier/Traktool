4dc71c97d0f365f96c4c0fe6a0ee7aa30b1d7166
Dylan Peltier <dylandavidpeltier@gmail.com>
2022-08-09 23:58:20 -0400

    expanded mobile responsiveness + new landing page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,22 +2,9 @@ import React from 'react';
 import classes from './HomePage.module.scss';
 
 const HomePage = () => {
-	return (
-		<div className={classes.middletext}>
-			<div className={classes.toptext}>
-				<h2>
-					{' '}
-					<span className={classes.h1orange}>t</span>he <br></br> &nbsp;conve<span className={classes.h2green}>r</span>gence of &nbsp;&nbsp;cre<span className={classes.h2green}>a</span>tivity  & practi<span className={classes.h2green}>k</span>ality &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				</h2>
-			</div>
-			<div className={classes.bottomtext}>
-				<h1>
-					{' '}
-					
-				</h1>
-			</div>
-		</div>
-	);
+	return <div className={classes.svg}>
+		<div className={classes.image}></div>
+	</div>;
 };
 
 export default HomePage;
